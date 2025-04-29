90ae05b24c9c48aedaade1a8eb83f680aed2ade6
AngelASC6 <Angelmichaelvasquez@gmail.com>
2025-04-29 18:03:37 -0400

    undid demo

--- personal-website/src/components/AboutMe.js
+++ personal-website/src/components/AboutMe.js
@@ -9,7 +9,8 @@ export default function AboutMe() {
         <div id="WhoAmI" className="flex flex-col items-center justify-center w-1/2 mx-10 self-start pt-12">
           <div>
             <p className="text-[#393939] text-lg text-center">
-TEST              <br /><br />
+              Hi, I’m Angel, a fourth-year student at RIT majoring in technology and design. I’m passionate about web development and have gained hands-on experience using technologies like React and Tailwind. I also have a strong foundation in cloud computing, having earned my Azure Fundamentals certification, and I'm always looking to deepen my technical expertise through projects and coursework.
+              <br /><br />
               In addition to my web development work, I’m focused on creating intuitive user interfaces and exploring ways to integrate APIs into digital experiences. I strive to write clean, efficient code while ensuring the end product is user-friendly and accessible.
               <br /><br />
               Right now, I’m actively seeking a summer internship where I can apply my skills and continue learning in a professional setting. If you’re looking for someone with a strong technical background and a passion for design, feel free to get in touch. I’d love to connect and discuss potential opportunities.
